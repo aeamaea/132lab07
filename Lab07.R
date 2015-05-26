df7b56f7b63a1b1914454bdfc77d394f1c873b53
aeamaea <amr.malik@gmail.com>
2015-05-26 14:18:22 -0400

    added upto task 08

--- Lab07.R
+++ Lab07.R
@@ -40,3 +40,77 @@ collatzIter <- function(startValue=10, numTimes=3) {
     
     return(retVect)
 }
+
+## Task5 :: iterate collatz function stop at 1 
+T5_collatzIter1 <- function(startValue=10) {
+ 
+    if (startValue==10 ) 
+    {
+        print("Called with defaults. Start=10 ...")
+    }
+    
+    retVect <- vector(mode="integer", length = 0) ## initvector
+    retVect <- c(retVect,startValue) ## insert startValue
+    
+    ## keep going until the collatz output reaches 1
+    while ((retVect[length(retVect)] > 1)) {
+        retVect <- c(retVect,collatz(retVect[length(retVect)]))
+        ## if(retVect[length(retVect)] == 1) return(retVect)
+    }
+    
+    return(retVect)
+}
+
+## Task6 :: for a startValue, return number of tries, orbit by iterating collatz function stop at 1 
+T6_collatzIter <- function(startValue=10) {
+    
+    if (startValue==10 ) 
+    {
+        print("Called with defaults. Start=10 ...")
+    }
+    
+    retVect <- vector(mode="integer", length = 0) ## initvector
+    retVect <- c(retVect,startValue) ## insert startValue
+    
+    numTries <- 0
+    
+    ## keep going until the collatz output reaches 1
+    while ((retVect[length(retVect)] > 1)) {
+        numTries <- numTries + 1
+        retVect <- c(retVect,collatz(retVect[length(retVect)]))
+        ## if(retVect[length(retVect)] == 1) return(retVect)
+    }
+    
+    ## return a list, first sublist is the number of tries (numTries)
+    ## second has the orbit vector (numVect)
+    return(list(numTries,retVect))
+}
+
+## Task7 :: for a startValue, return number of tries, orbit by iterating collatz function stop at 1 
+Task7 <- function(inputVect=c(10,5,4)) {
+    
+    if (sum(inputVect==c(10,5,4)) == 3 ) 
+    {
+        print("Called with defaults. c(10,5,4) ...")
+    }
+    
+    retVect <- vector(mode="integer", length = 0) ## initvector
+    
+    ## T6_collatzIter() returns a list, first sublist has the no. of tries
+    ## we call T6_collatzIter() and index into the first sublist
+    numTries <- T6_collatzIter(inputVect[1])[[1]]
+    retVect <- c(retVect,numTries)) ## insert startValue
+    
+    
+    
+    ## keep going until the collatz output reaches 1
+
+    for (i in 1:length(inputVect)-1) {
+        numTries <- T6_collatzIter(inputVect[1])[[1]]
+        retVect <- c(retVect,numTries)) ## insert startValue
+    }
+    
+    ## construct data frame 
+
+    return(data.frame(inputVect,retVect))
+}
